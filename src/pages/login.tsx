08326a52c6096a27d380db05aa65a354197c885b
Tana <tanabhumeatwork@gmail.com>
2023-05-21 01:18:30 +0900

    content adjustment (#8)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -13,11 +13,11 @@ const Home: NextPage = () => {
             <label htmlFor="Email" className="text-sm-semibold text-gray-600 mb-[6px]">Email</label>
             <input type="email" id="email" placeholder="Enter your email" className="mb-[20px] h-[44px] px-4 w-full border-[1px] border-gray-100 rounded-[8px]"/>
              <label htmlFor="Password" className="text-sm-semibold text-gray-600 mb-[6px]">Password</label>
-            <input type="password" id="password" placeholder="" className="mb-[24px] h-[44px] px-4 w-full border-[1px] border-gray-100 rounded-[8px]"/>
+            <input type="password" id="password" placeholder="Enter your password" className="mb-[24px] h-[44px] px-4 w-full border-[1px] border-gray-100 rounded-[8px]"/>
             <button className="button-primary mb-[16px]">Log in</button>
             <div className="button-secondary flex flex-row items-center justify-center mb-[32px]">
                 <Image src="/icons/GoogleIcon.png" alt="" width="24" height="24" className="mr-[12px]"/>
-                <div>Sign in with Google</div>
+                <div>Log in with Google</div>
             </div>
             <div className="text-sm-regular text-gray-500 text-center">
                 {/* eslint-disable-next-line react/no-unescaped-entities */}
