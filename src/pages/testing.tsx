a33e83e62305fc03486f8eaf88794d40e98695fd
Tana <tanabhumeatwork@gmail.com>
2023-05-21 01:51:56 +0900

    - Created menu bar - Imported relevant icons
    Bugs
    - Misaligned icons

--- src/pages/testing.tsx
+++ src/pages/testing.tsx
@@ -1,13 +1,10 @@
 import { type NextPage } from "next";
-
+import MenuBar from "~/pages/components/MenuBar";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-screen">
-          <div className="display-md-semibold">This is a display text</div>
-          <div className="rounded-xl bg-green-500 text-lg-bold px-6 py-2 mt-2">This is a text</div>
-      </div>
+      <MenuBar location={4}/>
     </>
   );
 };
