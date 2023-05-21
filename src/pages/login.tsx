c2fe348105586c6b2711e7baa8aa595ca9152f0c
AkritW <mail2max.w@gmail.com>
2023-05-22 02:14:56 +0800

    integreate login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,44 +1,93 @@
-import { type NextPage } from "next";
-import React, {type ChangeEvent} from "react";
-import Image from 'next/image'
-import InitialScreen from "~/pages/components/InitialScreen";
-import Link from 'next/link'
-import {useState} from "react"
-import {HeadBar} from "~/pages/components/HeadBar";
+import { type NextPage } from "next"
+import React, { type ChangeEvent } from "react"
+import Image from "next/image"
+import InitialScreen from "~/pages/components/InitialScreen"
+import Link from "next/link"
+import { useState } from "react"
+import { HeadBar } from "~/pages/components/HeadBar"
 
 
+
+interface AuthStatus {
+      auth: boolean
+    }
 const Home: NextPage = () => {
-  const[inputValue, setInputValue] = useState('')
-  const handleInputChange = (event:ChangeEvent) => {
-      setInputValue((event.target as HTMLTextAreaElement).value )
-      console.log(inputValue)
+  const [inputValue, setInputValue] = useState("")
+  const handleInputChange = (event: ChangeEvent) => {
+    setInputValue((event.target as HTMLTextAreaElement).value)
+    console.log(inputValue)
   }
 
-  const handleClick = () => {
-      console.log(inputValue)
+  const handleSubmit = async () => {
+    const res = await fetch("/api/checkAuth/", {
+      method: "POST",
+      body: JSON.stringify({
+        email: inputValue,
+        password: inputValue, // todo: where is input value for password
+      }),
+    })
+
+    const resJson = (await res.json()) as AuthStatus
+    const isLogin: boolean = resJson.auth
+
+    // todo: handle popup/redirect
   }
 
   return (
     <>
-        <HeadBar />
-        <InitialScreen string={'Log in to your account'}/>
-        <div className="mx-[20px] flex flex-col">
-            <label htmlFor="Email" className="text-sm-semibold text-gray-600 mb-[6px]">Email</label>
-            <input type="email" id="email" placeholder="Enter your email" onChange={handleInputChange} className="mb-[20px] h-[44px] px-4 w-full border-[1px] border-gray-100 rounded-[8px]"/>
-             <label htmlFor="Password" className="text-sm-semibold text-gray-600 mb-[6px]">Password</label>
-            <input type="password" id="password" placeholder="Enter your password" className="mb-[24px] h-[44px] px-4 w-full border-[1px] border-gray-100 rounded-[8px]"/>
-            <button onClick={handleClick} className="button-primary mb-[16px]">Log in</button>
-            <div className="button-secondary flex flex-row items-center justify-center mb-[32px]">
-                <Image src="/icons/GoogleIcon.png" alt="" width="24" height="24" className="mr-[12px]"/>
-                <div>Log in with Google</div>
-            </div>
-            <div className="text-sm-regular text-gray-500 text-center">
-                {/* eslint-disable-next-line react/no-unescaped-entities */}
-                <div>Don't have an account? <span className="inline text-sm-semibold text-orange-600"><Link href="/signup">Sign up</Link></span></div>
-            </div>
+      <HeadBar />
+      <InitialScreen string={"Log in to your account"} />
+      <div className="mx-[20px] flex flex-col">
+        <label
+          htmlFor="Email"
+          className="text-sm-semibold mb-[6px] text-gray-600"
+        >
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Enter your email"
+          onChange={handleInputChange}
+          className="mb-[20px] h-[44px] w-full rounded-[8px] border-[1px] border-gray-100 px-4"
+        />
+        <label
+          htmlFor="Password"
+          className="text-sm-semibold mb-[6px] text-gray-600"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter your password"
+          className="mb-[24px] h-[44px] w-full rounded-[8px] border-[1px] border-gray-100 px-4"
+        />
+        <button onClick={handleSubmit} className="button-primary mb-[16px]">
+          Log in
+        </button>
+        <div className="button-secondary mb-[32px] flex flex-row items-center justify-center">
+          <Image
+            src="/icons/GoogleIcon.png"
+            alt=""
+            width="24"
+            height="24"
+            className="mr-[12px]"
+          />
+          <div>Log in with Google</div>
         </div>
+        <div className="text-sm-regular text-center text-gray-500">
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          <div>
+            Don't have an account?{" "}
+            <span className="text-sm-semibold inline text-orange-600">
+              <Link href="/signup">Sign up</Link>
+            </span>
+          </div>
+        </div>
+      </div>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
