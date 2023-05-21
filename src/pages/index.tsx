7bf9d75485af7376f6c578e2b87890698978ed1c
Bluebanana404 <118740992+Bluebanana404@users.noreply.github.com>
2023-05-21 17:48:13 +0900

    add headbar (#15)
    * - Created HearBarSecondary component
    - Imported relevant icons
    
    * - Created team.tsx
    - Imported relevant images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,6 +38,16 @@ const Home: NextPage = () => {
                 to deploy it.
               </div>
             </Link>
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="/login"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Login page →</h3>
+              <div className="text-lg">
+                Access app
+              </div>
+            </Link>
           </div>
         </div>
       </main>
