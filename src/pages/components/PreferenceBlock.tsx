9b5abb6a7a36587c9bd05c2640f5089ac74ee06c
Tana <tanabhumeatwork@gmail.com>
2023-05-22 14:08:28 +0900

    - Finalized profile.tsx - Finalized PreferenceBlock.tsx
    Ready to integrate

--- src/pages/components/PreferenceBlock.tsx
+++ src/pages/components/PreferenceBlock.tsx
@@ -1,20 +1,23 @@
 import Image from 'next/image'
 
-// eslint-disable-next-line react/display-name
-export default PreferenceBlock = (category:string, text:string) => {
-    const map = new Map<string, string[]>([
+const map = new Map<string, string[]>([
         ["travel", ["blue", "suitcase"]],
         ["game", ["yellow", "game"]],
         ["scenery", ["green", "mountain"]],
         ["music", ["red", "music"]],
         ["computer", ["red", "coding"]]
     ])
-    map.get(category)
+const PreferenceBlock = ({category, text}:{category:string, text:string}) => {
+    const components = map.get(category)
+    const classOne = `px-[8px] h-[36px] bg-${components[0]}-25 rounded-[4px]`
+    const classTwo = `text-small-regular text-${components[0]}-600 translate-y-[5px] inline-block`
 
     return (
-        <div className="px-[8px] h-[36px] bg-green-25 rounded-[4px]">
-            <Image className="-translate-y-[4px] inline-block mr-[8px]" src="/icons/mountain.png" alt="" width={20} height={20}></Image>
-            <p className="text-small-regular text-green-600 inline-block">Hiking</p>
+        <div className={classOne}>
+            <Image className="translate-y-[2px] inline-block mr-[8px]" src="/icons/mountain.png" alt="" width={20} height={20}></Image>
+            <p className={classTwo}>{text}</p>
         </div>
     )
 }
+
+export {PreferenceBlock}
