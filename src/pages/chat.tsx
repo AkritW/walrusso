96be3f1f642749f04b73a539fea1ef2b2849e8cd
Tana <tanabhumeatwork@gmail.com>
2023-05-24 06:50:47 +0900

    - Created chat components

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,34 +1,92 @@
-import { type NextPage } from "next"
-import MenuBar from "~/pages/components/MenuBar"
-import { HeadBarSecondary } from "~/pages/components/HeadBar"
-import Image from "next/image"
-import Link from "next/link"
-import { useEffect, useState } from "react"
-import Text from "./components/Text"
+import { type NextPage } from "next";
+import MenuBar from "~/pages/components/MenuBar";
+import { HeadBarSecondary } from "~/pages/components/HeadBar";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Text from "./components/Text";
 
 interface _ChatAPIResponse {
-  from: string
-  time: string
-  text: string
+  from: string;
+  time: string;
+  text: string;
 }
 
-type ChatAPIResponse = Array<_ChatAPIResponse>
+type ChatAPIResponse = Array<_ChatAPIResponse>;
 
 const Home: NextPage = () => {
-  const [chat, setChat] = useState<ChatAPIResponse | null>(null)
+  const [chat, setChat] = useState<ChatAPIResponse | null>(null);
 
   useEffect(() => {
     void (async () => {
-      const response = await fetch("/api/getChat")
-      console.log(response)
-      const data = (await response.json()) as ChatAPIResponse
-      console.log(data)
+      const response = await fetch("/api/getChat");
+      console.log(response);
+      const data = (await response.json()) as ChatAPIResponse;
+      console.log(data);
 
-      setChat(data)
-    })()
-  }, [])
+      setChat(data);
+    })();
+  }, []);
 
-  // todo: turn into components
+  const singleText = (name: string, image: string, message: string) => {
+    return (
+      <div className=" mt-[24px] flex flex-col">
+        <div className="flex flex-row">
+          <Image
+            src={`/teamMembers/${image}.png`}
+            width={32}
+            height={32}
+            alt=""
+            className="h-[32px] self-end"
+          />
+          <div className="flex-flex-row ml-[13px]">
+            <p className="text-xs-semibold pl-[12px] text-gray-700">{name}</p>
+            <p className="text-sm-regular rounded-[39px] bg-white px-[16px] py-[6px] text-gray-700">
+              {message}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const doubleText = (
+    name: string,
+    image: string,
+    message: string,
+    messageTwo: string
+  ) => {
+    return (
+      <div className="mt-[24px] flex flex-col">
+        <div className="flex flex-row">
+          <Image
+            src={`/teamMembers/${image}.png`}
+            width={32}
+            height={32}
+            alt=""
+            className="h-[32px] self-end"
+          />
+          <div className="flex-flex-row ml-[13px]">
+            <p className="text-xs-semibold pl-[12px] text-gray-700">{name}</p>
+            <p className="text-sm-regular mb-[2px] w-fit rounded-[39px] bg-white px-[16px] py-[6px] text-gray-700">
+              {message}
+            </p>
+            <p className="text-sm-regular w-fit rounded-[39px] bg-white px-[16px] py-[6px] text-gray-700">
+              {messageTwo}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const selfText = (message: string) => {
+    return (
+      <p className="text-sm-regular mt-[2px] w-fit self-end rounded-[39px] bg-red-400 px-[16px] py-[6px] text-white">
+        {message}
+      </p>
+    );
+  };
 
   return (
     <>
@@ -46,30 +104,15 @@ const Home: NextPage = () => {
             />
           </Link>
         </div>
-        <div className="mx-[16px] mt-[24px] flex flex-col">
-          <div className="flex flex-row">
-            <Image
-              src="/teamMembers/Member.png"
-              width={32}
-              height={32}
-              alt=""
-              className="h-[32px] self-end"
-            />
-            <div className="flex-flex-row ml-[13px]">
-              <p className="text-xs-semibold pl-[12px] text-gray-700">
-                Candice
-              </p>
-              <p className="text-sm-regular rounded-[39px] bg-white px-[16px] py-[6px] text-gray-700">
-                When do we want to have dinner?
-              </p>
-              <p className="text-sm-regular mt-[2px] w-fit rounded-[39px] bg-white px-[16px] py-[6px] text-gray-700">
-                16:00 Tuesday?
-              </p>
-            </div>
-          </div>
-          <p className="text-sm-regular mt-[2px] w-fit self-end rounded-[39px] bg-red-400 px-[16px] py-[6px] text-white">
-            16:00 Tuesday?
-          </p>
+        <div className="mx-[16px] mt-[24px] flex flex-col space-y-[16px]">
+          {doubleText(
+            "Candice",
+            "Member",
+            "When do we want to have dinner?",
+            "16:00 Tuesday?"
+          )}
+          {selfText("Sounds good!")}
+          {singleText("Arm", "Member-2", "Wassup Beijing")}
         </div>
         <div className="absolute bottom-[100px] ml-[16px] flex h-[44px] w-[358px] items-center rounded-[35px] bg-white text-center">
           <p className="text-lg-regular ml-[16px] text-gray-500">Aa</p>
@@ -80,7 +123,7 @@ const Home: NextPage = () => {
       </div>
       <MenuBar location={4} />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
