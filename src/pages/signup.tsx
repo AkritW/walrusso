03f270afe85d448ee0b9a321553aa11574175787
Tana <tanabhumeatwork@gmail.com>
2023-05-23 19:43:12 +0900

    signup.tsx - Added data submission - Added email validation - Added password validation (match) - Added error messages

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,54 +1,146 @@
 import { type NextPage } from "next";
-import React from "react";
-import Image from 'next/image'
+import React, { ChangeEvent } from "react";
+import Image from "next/image";
 import InitialScreen from "~/pages/components/InitialScreen";
-import Link from 'next/link'
-import {HeadBar} from "~/pages/components/HeadBar";
+import Link from "next/link";
+import { useState } from "react";
+import { HeadBar } from "~/pages/components/HeadBar";
+import { useRouter } from "next/router";
 
 interface CreateStatus {
-    acknowledged: boolean,
-    insertedId: string 
+  acknowledged: boolean;
+  insertedId: string;
 }
 
-
 const Home: NextPage = () => {
-  // todo: where is the input value
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setpasswordValue] = useState("");
+  const [passwordReValue, setpasswordReValue] = useState("");
+  const [validMail, setvalidMail] = useState(false);
+  const [validPass, setvalidPass] = useState(false);
+  const router = useRouter();
+
+  const handleEmailChange = (event: ChangeEvent) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const input = (event.target as HTMLTextAreaElement).value;
+    if (emailPattern.test(input)) {
+      setEmailValue(input);
+      setvalidMail(true);
+    } else {
+      setvalidMail(false);
+    }
+  };
+
+  const handlePasswordChange = (event: ChangeEvent) => {
+    const pass = (event.target as HTMLTextAreaElement).value;
+    setpasswordValue(pass);
+    if (passwordValue == passwordReValue) {
+      setvalidPass(true);
+    } else {
+      setvalidPass(false);
+    }
+  };
+
+  const handlePasswordRepeat = (event: ChangeEvent) => {
+    const pass = (event.target as HTMLTextAreaElement).value;
+    setpasswordReValue(pass);
+    if (passwordValue == passwordReValue) {
+      setvalidPass(true);
+    } else {
+      setvalidPass(false);
+    }
+  };
 
   const handleSubmit = async () => {
     const res = await fetch("/api/createUser/", {
       method: "POST",
       body: JSON.stringify({
-        email: "",
-        password: "", // todo: where is input value for password
-      })
-    })
-    const resJson = res.json() as CreateStatus
-    const isCreated = resJson.acknowledged
-
-    // todo: handle popup/redirect
-  }
+        email: emailValue,
+        password: passwordValue,
+      }),
+    });
+    const resJson = res.json() as CreateStatus;
+    const isCreated = resJson.acknowledged;
+    router.push("/team");
+  };
 
   return (
     <>
-        <HeadBar />
-        <InitialScreen string={'Create an account'}/>
-        <div className="mx-[20px] flex flex-col">
-            <label htmlFor="Email" className="text-sm-semibold text-gray-600 mb-[6px]">Email</label>
-            <input type="email" id="email" placeholder="Enter your email" className="mb-[20px] h-[44px] px-4 w-full border-[1px] border-gray-100 rounded-[8px]"/>
-            <label htmlFor="Password" className="text-sm-semibold text-gray-600 mb-[6px]">Password</label>
-            <input type="password" id="password" placeholder="Enter your password" className="mb-[20px] h-[44px] px-4 w-full border-[1px] border-gray-100 rounded-[8px]"/>
-            <label htmlFor="Password" className="text-sm-semibold text-gray-600 mb-[6px]">Re-enter password</label>
-            <input type="password" id="password" placeholder="Enter your password" className="mb-[24px] h-[44px] px-4 w-full border-[1px] border-gray-100 rounded-[8px]"/>
-            <button className="button-primary mb-[16px]">Sign up</button>
-            <div className="button-secondary flex flex-row items-center justify-center mb-[32px]">
-                <Image src="/icons/GoogleIcon.png" alt="" width="24" height="24" className="mr-[12px]"/>
-                <div>Sign up with Google</div>
-            </div>
-            <div className="text-sm-regular text-gray-500 text-center">
-                {/* eslint-disable-next-line react/no-unescaped-entities */}
-                <div>Already have an account? <span className="inline text-sm-semibold text-orange-600"><Link href="/login">Log in</Link></span></div>
-            </div>
+      <HeadBar />
+      <InitialScreen string={"Create an account"} />
+      <div className="mx-[20px] flex flex-col">
+        <label
+          htmlFor="Email"
+          className="text-sm-semibold mb-[6px] text-gray-600"
+        >
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Enter your email"
+          onChange={handleEmailChange}
+          className="h-[44px] w-full rounded-[8px] border-[1px] border-gray-100 px-4"
+        />
+        <p className="text-sm-bold text-orange-600" hidden={validMail}>
+          Invalid email
+        </p>
+        <label
+          htmlFor="Password"
+          className="text-sm-semibold mb-[6px] mt-[20px] text-gray-600"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter your password"
+          onChange={handlePasswordChange}
+          className="h-[44px] w-full rounded-[8px] border-[1px] border-gray-100 px-4"
+        />
+        <p className="text-sm-bold text-orange-600" hidden={!validPass}>
+          Passwords do not match
+        </p>
+        <label
+          htmlFor="Password"
+          className="text-sm-semibold mb-[6px] mt-[20px] text-gray-600"
+        >
+          Re-enter password
+        </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Re-enter your password"
+          onChange={handlePasswordRepeat}
+          className="mb-[24px] h-[44px] w-full rounded-[8px] border-[1px] border-gray-100 px-4"
+        />
+        <button
+          onClick={handleSubmit}
+          className="button-primary mb-[16px]"
+          disabled={!validMail && !validPass}
+        >
+          Sign up
+        </button>
+        <div className="button-secondary mb-[32px] flex flex-row items-center justify-center">
+          <Image
+            src="/icons/GoogleIcon.png"
+            alt=""
+            width="24"
+            height="24"
+            className="mr-[12px]"
+          />
+          <div>Sign up with Google</div>
         </div>
+        <div className="text-sm-regular text-center text-gray-500">
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          <div>
+            Already have an account?{" "}
+            <span className="text-sm-semibold inline text-orange-600">
+              <Link href="/login">Log in</Link>
+            </span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
