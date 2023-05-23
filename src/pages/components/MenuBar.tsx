279f2d1fb7417dbe29e39fdca95fa792af427472
Tanabhume <tanabhumeatwork@gmail.com>
2023-05-23 14:04:31 +0900

    - Completed chat.tsx - Added relevant icons - Bug fix on MenuBar.tsx

--- src/pages/components/MenuBar.tsx
+++ src/pages/components/MenuBar.tsx
@@ -10,10 +10,10 @@ export default function MenuBar({location}:{location:number}) {
                 <Image alt="" src={(location == 4 ? '/icons/chatOn.png' : '/icons/chat.png')} width="0" height="0" className="w-[32px] h-[auto]" sizes="100vw"/>
             </div>
             <div className="flex flex-row w-screen justify-around -mt-[10px]">
-                <p className={`menu-item text-gray-500 ${location == 1 ? 'text-orange-400' : ''}`}>Team</p>
-                <p className={`menu-item text-gray-500 ${location == 2 ? 'text-orange-400' : ''}`}>Quests</p>
-                <p className={`menu-item text-gray-500 ${location == 3 ? 'text-orange-400' : ''}`}>Rank</p>
-                <p className={`menu-item text-gray-500 ${location == 4 ? 'text-orange-400' : ''}`}>Chats</p>
+                <p className={`menu-item text-gray-500 w-[80px] text-center ${location == 1 ? 'text-orange-400' : ''}`}>Team</p>
+                <p className={`menu-item text-gray-500 w-[80px] text-center ${location == 2 ? 'text-orange-400' : ''}`}>Quests</p>
+                <p className={`menu-item text-gray-500 w-[80px] text-center ${location == 3 ? 'text-orange-400' : ''}`}>Rank</p>
+                <p className={`menu-item text-gray-500 w-[80px] text-center ${location == 4 ? 'text-orange-400' : ''}`}>Chats</p>
             </div>
         </div>
     )
