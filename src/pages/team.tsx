f4a8a762f980b94bdba45da5998071605c518c88
Tana <tanabhumeatwork@gmail.com>
2023-05-24 03:16:41 +0900

    - Added profile component - Added demo data

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,78 +1,63 @@
 import { type NextPage } from "next";
 import MenuBar from "~/pages/components/MenuBar";
-import {HeadBarSecondary} from "~/pages/components/HeadBar";
-import Image from 'next/image'
+import { HeadBarSecondary } from "~/pages/components/HeadBar";
+import Image from "next/image";
+import React, { useState } from "react";
+
+interface teamInfo {
+  name: string;
+  imagePath: string;
+}
+
+const MyComponent: React.FC<teamInfo> = ({ name, imagePath }) => {
+  return (
+    <>
+      <div className="col-span-1 flex flex-col items-center">
+        <Image
+          src={`/teamMembers/${imagePath}.png`}
+          width={"64"}
+          height={"64"}
+          alt={""}
+        />
+        <div className="text-sm-regular mt-[8px]">{name}</div>
+      </div>
+    </>
+  );
+};
+
+const demo = [
+  ["Olivia", "Member"],
+  ["Phoenix", "Member-0"],
+  ["Lana", "Member-1"],
+  ["Demi", "Member-2"],
+];
+
+const profiles = demo.map((e) => (
+  <MyComponent
+    name={e[0] as string}
+    imagePath={e[1] as string}
+    key={e[0] as string}
+  />
+));
 
 const Home: NextPage = () => {
   return (
     <>
-      <HeadBarSecondary heading={"Your Team"}/>
-        <div className="flex justify-center w-screen h-screen bg-orange-25">
-            <div className="mt-[23px] w-[352px] h-[578px] bg-white flex flex-col items-center rounded-[16px] shadow-lg">
-                <div className={"mt-[8px] display-sm-semibold text-gray-900"}>Team Walrus</div>
-                <div className={"mt-[8px] text-sm-regular text-gray-900"}>From 1 - 31 May 2023</div>
-                <div className="grid grid-cols-4 gap-x-[24px] gap-y-[32px] mt-[16px]">
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Olivia</div>
-                    </div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-0.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Phoenix</div>
-                    </div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-1.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">lana</div>
-                    </div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-2.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Demi</div>
-                    </div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-3.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Zahir</div>
-                    </div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-4.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Eve</div>
-                    </div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-5.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Drew</div>
-                    </div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-6.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Natali</div>
-                    </div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-7.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Candice</div>
-                    </div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-8.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Orlando</div>
-                    </div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-9.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Koray</div>
-                    </div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-10.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Ava</div>
-                    </div>
-                    <div></div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-11.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Kate</div>
-                    </div>
-                    <div className="col-span-1 flex flex-col items-center">
-                        <Image src={"/teamMembers/Member-12.png"} width={"64"} height={"64"} alt={""}/>
-                        <div className="text-sm-regular mt-[8px]">Andi</div>
-                    </div>
-                </div>
-            </div>
+      <HeadBarSecondary heading={"Your Team"} />
+      <div className="flex h-screen w-screen justify-center bg-orange-25">
+        <div className="mt-[23px] flex h-[578px] w-[352px] flex-col items-center rounded-[16px] bg-white shadow-lg">
+          <div className={"display-sm-semibold mt-[8px] text-gray-900"}>
+            Team Walrus
+          </div>
+          <div className={"text-sm-regular mt-[8px] text-gray-900"}>
+            From 1 - 31 May 2023
+          </div>
+          <div className="mt-[16px] grid grid-cols-4 gap-x-[24px] gap-y-[32px]">
+            {profiles}
+          </div>
         </div>
-      <MenuBar location={1}/>
+      </div>
+      <MenuBar location={1} />
     </>
   );
 };
