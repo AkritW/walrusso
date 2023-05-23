da96bc6ac25253dffd12396ebc202ffed6a71d5b
Akrit Woranithiphong <mail2max.w@gmail.com>
2023-05-23 16:34:29 +0800

    add getInterest endpoint (#43)

--- src/pages/preferences.tsx
+++ src/pages/preferences.tsx
@@ -1,34 +1,56 @@
-import { type NextPage } from "next";
-import {HeadBar} from "~/pages/components/HeadBar";
-import Image from 'next/image'
-import {PreferenceBlock} from "~/pages/components/PreferenceBlock";
+import { type NextPage } from "next"
+import { HeadBar } from "~/pages/components/HeadBar"
+import Image from "next/image"
+import { useEffect } from "react"
+import { PreferenceBlock } from "~/pages/components/PreferenceBlock"
+
+interface InterestAPIResponse {
+  email: string
+  options: Array<string>
+}
 
 const Home: NextPage = () => {
-    const createTags = (tags:string[]) => {
-        let result = ""
-        for (const tag of tags){
-            const temp = PreferenceBlock(tag)
-            result = String(result) + String(temp)
-        }
-        return "tags"
+  const createTags = (tags: string[]) => {
+    let result = ""
+    for (const tag of tags) {
+      const temp = PreferenceBlock(tag)
+      result = String(result) + String(temp)
     }
-    const manyTags = [PreferenceBlock("Pop"), PreferenceBlock("Mountain")]
-    // const manyTags = "banana"
-    return (
-      <>
-        <HeadBar/>
-        <div className="relative w-screen h-[774px]">
-            <div className="mx-[20px] flex flex-col items-center justify-center mt-[60px]">
-            <div className="display-xs-bold mb-[32px] text-gray-900">Preferences</div>
-            <div className="text-md-regular mb-[32px] text-grey-900">Select topics that match your preferences</div>
-            <div className="mx-[25px] w-[340px] flex flex-row justify-center gap-[8px]">
-                <div className="flex flex-row gap-[8px]">{manyTags}</div>
-            </div>
-            </div>
-            <button className="button-primary !w-[350px] absolute left-[20px] bottom-[178px]">Done</button>
+    return "tags"
+  }
+  const manyTags = [PreferenceBlock("Pop"), PreferenceBlock("Mountain")]
+
+  useEffect(() => {
+    void (async () => {
+      const response = await fetch("/api/interest")
+      console.log(response)
+      const interest = (await response.json()) as InterestAPIResponse
+
+      // todo: handle popup/redirect
+    })()
+  }, [])
+
+  return (
+    <>
+      <HeadBar />
+      <div className="relative h-[774px] w-screen">
+        <div className="mx-[20px] mt-[60px] flex flex-col items-center justify-center">
+          <div className="display-xs-bold mb-[32px] text-gray-900">
+            Preferences
+          </div>
+          <div className="text-md-regular text-grey-900 mb-[32px]">
+            Select topics that match your preferences
+          </div>
+          <div className="mx-[25px] flex w-[340px] flex-row justify-center gap-[8px]">
+            <div className="flex flex-row gap-[8px]">{manyTags}</div>
+          </div>
         </div>
-      </>
-    );
-  };
-  
-  export default Home;
+        <button className="button-primary absolute bottom-[178px] left-[20px] !w-[350px]">
+          Done
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Home
