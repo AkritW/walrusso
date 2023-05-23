c4f1d1451af40ffcb63dacde5fd3dcdbc27ada48
AkritW <mail2max.w@gmail.com>
2023-05-23 20:42:40 +0800

    add getChat endpoint

---
+++ src/pages/api/getChat.ts
@@ -0,0 +1,16 @@
+import { type NextApiRequest, type NextApiResponse } from "next"
+import { env } from "../../env.mjs"
+import { MongoClient } from "mongodb"
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const client = new MongoClient(env.DB_URL)
+
+  await client.connect()
+  const db = client.db("wulrusso")
+  const collection = db.collection("chat")
+  const dbRes = await collection.find().toArray()
+
+  res.status(200).json(dbRes)
+}
+
+export default handler
