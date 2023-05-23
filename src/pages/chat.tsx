1e8a1c39116b77f6339dce9aee26807ecebd7fb6
Akrit Woranithiphong <mail2max.w@gmail.com>
2023-05-24 02:20:09 +0800

    integrate chat page (#56)

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,41 +1,86 @@
-import { type NextPage } from "next";
-import MenuBar from "~/pages/components/MenuBar";
-import {HeadBarSecondary} from "~/pages/components/HeadBar";
-import Image from 'next/image'
-import Link from 'next/link';
+import { type NextPage } from "next"
+import MenuBar from "~/pages/components/MenuBar"
+import { HeadBarSecondary } from "~/pages/components/HeadBar"
+import Image from "next/image"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import Text from "./components/Text"
+
+interface _ChatAPIResponse {
+  from: string
+  time: string
+  text: string
+}
+
+type ChatAPIResponse = Array<_ChatAPIResponse>
 
 const Home: NextPage = () => {
+  const [chat, setChat] = useState<ChatAPIResponse | null>(null)
+
+  useEffect(() => {
+    void (async () => {
+      const response = await fetch("/api/getChat")
+      console.log(response)
+      const data = (await response.json()) as ChatAPIResponse
+      console.log(data)
+
+      setChat(data)
+    })()
+  }, [])
+
+  // todo: turn into components
+
   return (
     <>
-      <HeadBarSecondary heading={"Chats"}/>
-        <div className="w-screen h-[772px] bg-orange-100 relative">
-            <div className="bg-white h-[72px] w-screen relative flex items-center justify-center">
-                <p className="display-sm-regular text-gray-700">Team Pig</p>
-                <Link href="">
-                    <Image src="/icons/arrow.png" alt="" width={36} height={36} className="absolute top-[18px] left-0"/>
-                </Link>
-            </div>
-            <div className="flex flex-col mx-[16px] mt-[24px]">
-                <div className="flex flex-row">
-                    <Image src="/teamMembers/Member.png" width={32} height={32} alt="" className="h-[32px] self-end"/>
-                    <div className="flex-flex-row ml-[13px]">
-                        <p className="text-xs-semibold text-gray-700 pl-[12px]">Candice</p>
-                        <p className="px-[16px] py-[6px] bg-white text-sm-regular text-gray-700 rounded-[39px]">When do we want to have dinner?</p>
-                        <p className="px-[16px] py-[6px] bg-white text-sm-regular text-gray-700 rounded-[39px] w-fit mt-[2px]">16:00 Tuesday?</p>
-                    </div>
-                </div>
-                <p className="px-[16px] py-[6px] text-sm-regular text-white bg-red-400 rounded-[39px] w-fit mt-[2px] self-end">16:00 Tuesday?</p>
-            </div>
-            <div className="absolute bottom-[100px] w-[358px] ml-[16px] rounded-[35px] bg-white h-[44px] text-center flex items-center">
-                <p className="text-lg-regular text-gray-500 ml-[16px]">Aa</p>
-                <button onClick={""} className="absolute right-[16px]">
-                    <Image src="/icons/submit.png" alt="" height={24} width={24}/>
-                </button>
-            </div>
+      <HeadBarSecondary heading={"Chats"} />
+      <div className="relative h-[772px] w-screen bg-orange-100">
+        <div className="relative flex h-[72px] w-screen items-center justify-center bg-white">
+          <p className="display-sm-regular text-gray-700">Team Pig</p>
+          <Link href="">
+            <Image
+              src="/icons/arrow.png"
+              alt=""
+              width={36}
+              height={36}
+              className="absolute left-0 top-[18px]"
+            />
+          </Link>
         </div>
-      <MenuBar location={4}/>
+        <div className="mx-[16px] mt-[24px] flex flex-col">
+          <div className="flex flex-row">
+            <Image
+              src="/teamMembers/Member.png"
+              width={32}
+              height={32}
+              alt=""
+              className="h-[32px] self-end"
+            />
+            <div className="flex-flex-row ml-[13px]">
+              <p className="text-xs-semibold pl-[12px] text-gray-700">
+                Candice
+              </p>
+              <p className="text-sm-regular rounded-[39px] bg-white px-[16px] py-[6px] text-gray-700">
+                When do we want to have dinner?
+              </p>
+              <p className="text-sm-regular mt-[2px] w-fit rounded-[39px] bg-white px-[16px] py-[6px] text-gray-700">
+                16:00 Tuesday?
+              </p>
+            </div>
+          </div>
+          <p className="text-sm-regular mt-[2px] w-fit self-end rounded-[39px] bg-red-400 px-[16px] py-[6px] text-white">
+            16:00 Tuesday?
+          </p>
+        </div>
+        <div className="absolute bottom-[100px] ml-[16px] flex h-[44px] w-[358px] items-center rounded-[35px] bg-white text-center">
+          <p className="text-lg-regular ml-[16px] text-gray-500">Aa</p>
+          <button className="absolute right-[16px]">
+            <Image src="/icons/submit.png" alt="" height={24} width={24} />
+          </button>
+        </div>
+      </div>
+      <MenuBar location={4} />
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
---
+++ src/pages/components/Text.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+import React from "react"
+
+interface Props {
+  from: string
+  time: string
+  text: string
+}
+
+const Text: React.FC<Props> = (props) => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Text
