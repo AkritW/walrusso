f2946a2e3f081e2642d17d986d61820e4ff00f81
Bluebanana404 <118740992+Bluebanana404@users.noreply.github.com>
2023-05-23 17:18:40 +0900

    add preferences page (#36)
    * - Created layout for preferences.tsx
    - Adjusted PreferenceBlock.tsx
    
    Bugs
    - Block creation not functional
    
    * - Finalized PreferenceBlock.tsx
    - Updated prefences.tsx to match

--- src/pages/preferences.tsx
+++ src/pages/preferences.tsx
@@ -1,18 +1,31 @@
 import { type NextPage } from "next";
 import {HeadBar} from "~/pages/components/HeadBar";
 import Image from 'next/image'
+import {PreferenceBlock} from "~/pages/components/PreferenceBlock";
 
 const Home: NextPage = () => {
+    const createTags = (tags:string[]) => {
+        let result = ""
+        for (const tag of tags){
+            const temp = PreferenceBlock(tag)
+            result = String(result) + String(temp)
+        }
+        return "tags"
+    }
+    const manyTags = [PreferenceBlock("Pop"), PreferenceBlock("Mountain")]
+    // const manyTags = "banana"
     return (
       <>
         <HeadBar/>
-        <div className="mx-[20px] flex flex-col items-center justify-center mt-[60px]">
+        <div className="relative w-screen h-[774px]">
+            <div className="mx-[20px] flex flex-col items-center justify-center mt-[60px]">
             <div className="display-xs-bold mb-[32px] text-gray-900">Preferences</div>
-            <div className="display-md-medium mb-[32px] text-grey-900">Select topics that match your preferences</div>
-            <div className="w-full h-[168px] bg-orange-300 text-center text-middle">
-                tags will be here, will make components for it
+            <div className="text-md-regular mb-[32px] text-grey-900">Select topics that match your preferences</div>
+            <div className="mx-[25px] w-[340px] flex flex-row justify-center gap-[8px]">
+                <div className="flex flex-row gap-[8px]">{manyTags}</div>
             </div>
-            <button className="button-primary mt-[202px]">Done</button>
+            </div>
+            <button className="button-primary !w-[350px] absolute left-[20px] bottom-[178px]">Done</button>
         </div>
       </>
     );
