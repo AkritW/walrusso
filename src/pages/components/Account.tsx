98e2a393248963437819babece33570d9ce84b25
AkritW <mail2max.w@gmail.com>
2023-05-24 16:15:33 +0800

    integrate account page

--- src/pages/components/Account.tsx
+++ src/pages/components/Account.tsx
@@ -1,42 +1,62 @@
 import Image from "next/image";
 import MyComponent from "~/pages/components/PreferenceBlock";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const demo = [
-  "Michele Mazza",
-  "Techlead",
-  ["Mountain", "Yoga", "Pop"],
-  "Hi! I'm Michele Mazza",
-  "Member",
-];
+interface AccountAPIResponse {
+  name: string
+  occupation: string
+  department: string
+  preferences: Array<string>
+  bio: string 
+}
 
-const preferences = demo[2] as string[];
-const manyTags = preferences.map((e) => <MyComponent text={e} key={e} />);
-console.log(manyTags);
-const Account = () => {
+
+interface Props {
+  userId: string
+}
+
+const Account: React.FC<Props> = (props) => {
+  const [account, setAccount] = useState<AccountAPIResponse | null>(null)
+
+  useEffect(() => {
+    void (async () => {
+      const response = await fetch("/api/getAccount", {
+        method: "POST",
+        body: JSON.stringify({
+          id: props.userId
+        })
+      })
+      const data = (await response.json()) as AccountAPIResponse
+      console.log(data)
+
+      setAccount(data)
+    })()
+  }, [props.userId])
+
+  const manyTags = account && account.preferences.map((e) => <MyComponent text={e} key={e} />)
   return (
     <div className="absolute left-0 top-[72px] flex h-screen w-screen flex-col items-center justify-start bg-orange-25">
       <div className="mt-[48px] flex h-[616px] w-[334px] flex-col items-center rounded-[12px] bg-white px-[24px] shadow-2xl">
         <Image
-          src={`/teamMembers/${demo[4]}.png`}
+          src={`/teamMembers/member.png`}
           alt={""}
           width={128}
           height={128}
           className="mt-[32px] rounded-[64px]"
         />
-        <p className="display-sm-semibold mt-[12px] text-gray-800">{demo[0]}</p>
+        <p className="display-sm-semibold mt-[12px] text-gray-800">{account && account.name}</p>
         <p className="text-md-bold mt-[24px] self-start text-gray-800">
-          Department
+          {account && account.department}
         </p>
         <p className="text-lg-medium mt-[8px] self-start text-gray-800">
-          {demo[1]}
+          {account && account.occupation}
         </p>
         <p className="text-md-bold mb-[8px] mt-[24px] self-start text-gray-800">
-          Preferences
+          {account && account.preferences}
         </p>
         <div className="mb-[24px] flex flex-row justify-start">{manyTags}</div>
         <p className="text-md-bold self-start text-gray-800">Bio</p>
-        <p className="text-xs-regular self-start text-gray-800">{demo[3]}</p>
+        <p className="text-xs-regular self-start text-gray-800">{account && account.bio}</p>
       </div>
     </div>
   );
